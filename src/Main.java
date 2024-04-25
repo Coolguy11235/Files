278d6b1220d517b22913c9ef93a8205e2c49a05d
tim.safronov <tim.safronov@elev.ga.ntig.se>
2024-04-26 00:30:36 +0200

    Nu kan man hitta längsta ordet och vanligaste bokstaven i filerna words.txt och svenska-ord.txt

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import file.filHantering;
 
 public class Main {
     public static void main(String[] args) {
-
         new filHantering();
     }
 }
--- src/file/filHantering.java
+++ src/file/filHantering.java
@@ -1,62 +1,67 @@
 package file;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class filHantering {
+    Scanner scan = new Scanner(System.in);
     String textFileName = "text.txt";
     String personFileName = "personer.txt";
+    String wordsFileName = "words.txt";
+    String svenskaOrdFileName = "svenska-ord.txt";
 
     File textFileObject;
     File personFileObject;
+    File wordsFileObject;
+    File svenskaOrdFileObject;
     FileWriter myWriter;
 
     // Constructor
     public filHantering() {
-        textFileObject = new File(textFileName);
-        personFileObject = new File(personFileName);
-        write2File();
-        readFromTextFile();
-        readFromPersonFile();
-    }
+        System.out.println("Vilken fil vill du använda? (text, personer, words, ord)");
+        String val = scan.nextLine();
 
-    private void readFromTextFile() {
-        try {
-            Scanner textFileScanner = new Scanner(textFileObject);
-            System.out.println("Innehållet i text.txt: ");
-            while (textFileScanner.hasNextLine()) {
-                System.out.println(textFileScanner.nextLine());
-            }
-            textFileScanner.close();
-
-        } catch (FileNotFoundException e) {
-            System.out.println("text.txt kunde inte hittas!");
+        if (val.equalsIgnoreCase("text")) {
+            textFileObject = new File(textFileName);
+            write2File(textFileName);
+            readFromFile(textFileObject);
+        } else if (val.equalsIgnoreCase("personer")) {
+            personFileObject = new File(personFileName);
+            readFromFile(personFileObject);
+        } else if (val.equalsIgnoreCase("words")) {
+            findLongestWord(wordsFileName);
+            findMostCommonLetter(wordsFileName);
+        } else if (val.equalsIgnoreCase("ord")) {
+            findLongestWord(svenskaOrdFileName);
+            findMostCommonLetter(svenskaOrdFileName);
+        } else {
+            System.out.println("Ogiltig svar");
         }
-
     }
 
-    private void readFromPersonFile() {
+    private void readFromFile(File file) {
         try {
-            Scanner personFileScanner = new Scanner(personFileObject);
-            System.out.println("Innehållet i personer.txt: ");
-            while (personFileScanner.hasNextLine()) {
-                System.out.println(personFileScanner.nextLine());
+            Scanner fileScanner = new Scanner(file);
+            System.out.println("Innehållet i " + file.getName() + ": ");
+            while (fileScanner.hasNextLine()) {
+                System.out.println(fileScanner.nextLine());
             }
-            personFileScanner.close();
+            fileScanner.close();
 
         } catch (FileNotFoundException e) {
-            System.out.println("personer.txt kunde inte hittas!");
+            System.out.println(file.getName() + "kunde inte hittas!");
         }
 
     }
 
     // Skriver i en fil
-    private void write2File() {
+    private void write2File(String fileName) {
         try {
-            myWriter = new FileWriter(textFileName, true);
-            Scanner scan = new Scanner(System.in);
+            myWriter = new FileWriter(fileName, true);
 
             System.out.println("Vad vill du skriva i filen?");
             String input = scan.nextLine();
@@ -65,8 +70,68 @@ public class filHantering {
             myWriter.close();
 
         } catch (IOException e) {
-            System.out.println("Kunde inte skriva till text.txt!");
+            System.out.println("Kunde inte skriva till " + fileName + "!");
             e.printStackTrace();
+        }
+    }
+
+    private void findLongestWord (String fileName) {
+        try {
+            File fileObject = new File(fileName);
+            Scanner wordScanner = new Scanner(fileObject);
+
+            String longestWord = "";
+            while (wordScanner.hasNext()) {
+                String word = wordScanner.next();
+                if (word.length() > longestWord.length()) {
+                    longestWord = word;
+                }
+            }
+
+            if (!longestWord.isEmpty()) {
+                System.out.println("Det längsta ordet i " + fileName + " är: " + longestWord);
+            } else {
+                System.out.println(fileName + " innehåller inga ord");
+            }
+
+            wordScanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println(fileName + " kunde inte hittas!");
+        }
+    }
+
+    private void findMostCommonLetter (String fileName) {
+        try {
+            File fileObject = new File(fileName);
+            Scanner scan = new Scanner(fileObject);
+
+            Map<Character, Integer> lettercount = new HashMap<>();
+
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                for (char c : line.toCharArray()) {
+                    if (Character.isLetter(c)) {
+                        char lowercaseC = Character.toLowerCase(c);
+                        lettercount.put(lowercaseC, lettercount.getOrDefault(lowercaseC, 0) + 1);
+                    }
+                }
+            }
+
+            char mostCommonLetter = ' ';
+            int maxCount = 0;
+            for (Map.Entry<Character, Integer> entry : lettercount.entrySet()) {
+                if (entry.getValue() > maxCount) {
+                    mostCommonLetter = entry.getKey();
+                    maxCount = entry.getValue();
+                }
+            }
+
+            System.out.println("Den vanligaste bokstaven i " + fileName + " är: " + mostCommonLetter + " " + maxCount + " gånger.");
+
+            scan.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(fileName + " kunde inte hittas!");
         }
     }
 }
